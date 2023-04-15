92389f310e0f09bd3ba634fd9864f792e9cdd0e2
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-04-15 09:34:35 -0300

    define basic handlers

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "net/http"
+
+type fooHandler struct {
+	Message string
+}
+
+func (h *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(h.Message))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
 func main() {
-	// ...
+	http.Handle("/foo", &fooHandler{Message: "Hello World!"})
+
+	http.HandleFunc("/bar", barHandler)
+	http.ListenAndServe(":5000", nil)
 }
