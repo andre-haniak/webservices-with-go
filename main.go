b15ac8211ecdb6dfd35fecf22038db785106afde
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-04-15 16:36:28 -0300

    added file products.json with static products data

--- main.go
+++ main.go
@@ -1,45 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/andre-haniak/webservices-with-go/controllers"
-	"github.com/andre-haniak/webservices-with-go/models"
+	"time"
 )
 
-var productList []models.Product
-
-func init() {
-	productsJSON := `[
-		{
-			"productId": 1,
-			"manufacturer": "Johns-Jenkins",
-			"sku": "p5w705",
-			"upc": "939851000000",
-			"pricePerUnit": "81.29",
-			"quantityOnHand": 981,
-			"productName": "sticky note"
-		},
-		{
-			"productId": 2,
-			"manufacturer": "Kshlerin, Reinger and Beier",
-			"sku": "q7t864",
-			"upc": "939851000000",
-			"pricePerUnit": "85.60",
-			"quantityOnHand": 998,
-			"productName": "leg warmers"
-		}
-	]`
-	err := json.Unmarshal([]byte(productsJSON), &productList)
-	if err != nil {
-		log.Fatal(err)
-	}
+func middlewareHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("before handler; middleware start")
+		start := time.Now()
+		handler.ServeHTTP(w, r)
+		fmt.Printf("middleware finished; %s", time.Since(start))
+	})
 }
 
 func main() {
-	http.HandleFunc("/products", controllers.ProductsHandler)
-	http.HandleFunc("/products/", controllers.ProductHandler)
 	http.ListenAndServe(":5000", nil)
 }
